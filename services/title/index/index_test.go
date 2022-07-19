8bf031f6cf92f8b3f6c4671e221761ca928eb51a
Script <37535505+Scrip7@users.noreply.github.com>
2022-07-19 08:36:03 +0430

    test(titles): added more unit test cases

--- services/title/index/index_test.go
+++ services/title/index/index_test.go
@@ -6,8 +6,20 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestIndex(t *testing.T) {
-	res, err := Index("tt20256528")
+func TestMovie(t *testing.T) {
+	res, err := Index("tt20256528") // What Is a Woman?
+	assert.Nil(t, err)
+	assert.NotNil(t, res)
+}
+
+func TestTVSeries(t *testing.T) {
+	res, err := Index("tt0108778") // Friends
+	assert.Nil(t, err)
+	assert.NotNil(t, res)
+}
+
+func TestTVSeriesEpisode(t *testing.T) {
+	res, err := Index("tt0583436") // Friends S10.E1
 	assert.Nil(t, err)
 	assert.NotNil(t, res)
 }
